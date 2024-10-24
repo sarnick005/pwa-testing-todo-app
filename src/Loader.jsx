115e1ac7bc3d4a1427ba2c52bab598a9a603a0da
sarnick005 <sarnick.chakraborty@gmail.com>
2024-10-24 12:28:18 +0530

    Loader fixed

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,23 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Todo from "./Todo";
+import React from "react";
 
-const Loader = () => (
-  <div className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 flex items-center justify-center">
-    <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent"></div>
-  </div>
-);
-
-const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return loading ? <Loader /> : <Todo />;
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 p-4">
+      <div className="relative w-12 h-12 animate-spin">
+        <div className="absolute top-0 left-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute top-0 right-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute bottom-0 left-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute bottom-0 right-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+      </div>
+    </div>
+  );
 };
 
-export default App;
+export default Loader;
