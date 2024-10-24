9ccc933697d4a0ea9cd34569b32bf6deb6d44311
sarnick005 <sarnick.chakraborty@gmail.com>
2024-10-24 12:02:59 +0530

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Todo from "./Todo";
+import Loader from "./Loader";
 import "./index.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div className="App">{loading ? <Loader /> : <Todo />}</div>;
 };
 
 export default App;
---
+++ src/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="relative w-12 h-12 animate-spin">
+        <div className="absolute top-0 left-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute top-0 right-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute bottom-0 left-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+        <div className="absolute bottom-0 right-0 w-3 h-3 bg-gray-800 rounded-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
